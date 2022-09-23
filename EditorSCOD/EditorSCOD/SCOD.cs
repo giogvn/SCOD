f4c62d4243502f0adec3da8186e71f9aa301ff6b
unknown <cteds_07>
2022-09-23 20:50:39 -0300

    Salva o cońteúdo escrito na interface gráfica em um arquivo de texto
    Signed-off-by: unknown <cteds_07>

--- EditorSCOD/EditorSCOD/SCOD.cs
+++ EditorSCOD/EditorSCOD/SCOD.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace EditorSCOD
 {
     public partial class SCOD : Form
@@ -12,18 +14,38 @@ namespace EditorSCOD
 
         }
 
+        private void SaveFile()
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    Encoding unicode = Encoding.Unicode;
+                    myStream.Write(unicode.GetBytes(richTextBox1.Text));
+                    myStream.Close();
+                }
+            }
+
+        }
+
+     
+
         private void SCOD_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (richTextBox1.Text != "")
             {
-                // Display a MsgBox asking the user to save changes or abort.
                 if (MessageBox.Show("Do you want to save changes to your text?", "My Application",
                    MessageBoxButtons.YesNo) == DialogResult.Yes)
                 {
-                    // Cancel the Closing event from closing the form.
                     e.Cancel = true;
-                    // Call method to save file...
+                    this.SaveFile();
                 }
+                Application.Exit();
             }
 
         }
